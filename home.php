72bfa484138bcef9537eaa4b83084953d62bba0a
Jitendar E  Y <jitendarey9594@gmail.com>
2023-06-22 21:38:00 +0530

    new file:   about.php 	new file:   book.php 	new file:   book_form.php 	new file:   home.php 	new file:   package.php 	new file:   script.js 	new file:   style.css     commit;

---
+++ home.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
+/>
+ 
+
+
+
+
+
+
+
+
+
+
+<link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/style.css">
+
+    
+</head>
+<body>
+ 
+
+<section class = "header">
+<a href = "home.php" class = "logo"  >travel.</a>
+
+
+
+<nav class = "navbar">
+<a href="home.php">home</a>
+<a href="about.php">about</a>
+<a href="package.php">package</a>
+<a href="book.php">book</a>
+
+</nav>
+
+
+
+<div id="" class="fas fa-bars"></div>
+</section>
+
+
+
+
+
+
+
+
+<section class="fotter">
+
+<div class="box-container">
+
+<div class="box">
+  <h3>quick links</h3>
+<a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+<a href="about.php"> <i class="fas fa-angle-right"> about</a>
+<a href="package.php"><i class="fas fa-angle-right"> package</a>
+<a href="book.php"><i class="fas fa-angle-right"> book</a>
+
+</div>
+
+
+<div class="box">
+  <h3>extra links</h3>
+<a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+<a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+<a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+<a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+
+</div>
+
+
+<div class="box">
+  <h3>contact info</h3>
+<a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+<a href="#"> <i class="fas fa-phone"></i> +111-222-333</a>
+<a href="#"> <i class="fas fa-envelope"></i> travel21@gmail.com</a>
+<a href="#"> <i class="fas fa-map"></i> bengaluru, india - 400104</a>
+
+
+</div>
+
+<div class="box">
+  <h3>follow us</h3>
+  <a href="#"><i class="fab fa-facebook-f"></i> facebook</a>
+  <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+  <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+  <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>
+
+</div>
+
+</div>
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
